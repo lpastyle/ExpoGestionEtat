998a35a7ae75ac1e9ea1ce02d134055ce246aea0
lpastyle <lpastorelli@lpastyle.com>
2026-05-06 21:38:40 +0200

    01_variable_vs_state

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
+// EXEMPLE 1. 
+// variable ordinaire
+// L'interface NE SE MET PAS À JOUR quand compteur change
 export default function App() {
+  let compteur = 10;              // variable ordinaire
+
+  const incrementer = () => {
+    compteur = compteur + 1;     // incrémente la variable...
+    console.log(compteur);       
+  };
+  
+  // Affiche toujours 10 *
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.txtcpt}>{compteur}</Text>
+      <Button
+        title="incrémenter"
+        onPress={incrementer} />
+    </View>
+  ); 
+}
+
+// EXEMPlE 2.
+// Etat React Native
+// L'interface SE MET À JOUR automatiquement quand l'état change
+/* export default function App() {
+
+  // Déclaration d'un état React avec valeur initiale = 10
+  const [compteur, setCompteur] = useState(10);
+
+  const incrementer = () => {
+    setCompteur(compteur + 1);  // déclenche le rendu
+  };
+
+  // L'UI se met à jour automatiquement
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txtcpt}>{compteur}</Text>
+      <Button
+       title="incrémenter"
+       onPress={incrementer} />
     </View>
   );
-}
+} */
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +55,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txtcpt: {
+    fontSize: 40,
+    marginBottom: 24
+  }
 });
